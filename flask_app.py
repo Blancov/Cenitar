8896fff8661fdd57527bab06c46d3540bf8ee1b2
Blancov <ovelar.eduardo@outlook.com>
2024-10-18 12:43:35 -0700

    Updated get_prediction function to use the new OpenAI API

---
+++ flask_app.py
@@ -0,0 +1,8 @@
+def get_prediction(prompt):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt}],
+        max_tokens=100
+    )
+    return response['choices'][0]['message']['content'].strip()
+
